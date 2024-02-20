a5bdb4dece9e02814e65815a71238002fe1c12cf
Huynh Le Vu Dat <dat.hlv@suntech.local>
2024-02-20 23:32:34 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  server: {
-    "": {
-      target: "https://hlvudat1206.github.io/render-model-dp",
-      changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/api/, ""),
+  base: "/render-model-dp",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
