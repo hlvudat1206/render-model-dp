8c715f9b6ac3e23162c41b5daa32d5de93f43f86
Huynh Le Vu Dat <dat.hlv@suntech.local>
2024-02-21 00:49:32 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  // base: "/render-model-dp",
-  // resolve: {
-  //   alias: {
-  //     "@": path.resolve(__dirname, "./src"),
-  //   },
-  // },
+  base: "/render-model-dp",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   // server: {
   //   host: "127.0.0.1", // replace with the IP address of the Homestead machine
   //   https: false,
@@ -19,7 +19,10 @@ export default defineConfig({
   //     host: "127.0.0.1", // replace with the IP address of the Homestead machine
   //   },
   // },
-  server: {
-    origin: "http://127.0.0.1:5173",
-  },
+  // server: {
+  //   origin: "http://127.0.0.1:5173",
+  // },
+  // build: {
+  //   outDir: "../dist", // Change 'custom-dist' to your desired output directory
+  // },
 });
