d0fbeb75e3d3baa3b34e5559cb33ac26611d6568
Huynh Le Vu Dat <dat.hlv@suntech.local>
2024-02-21 00:02:50 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: "/render-model-dp",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  // base: "/render-model-dp",
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //   },
+  // },
 });
