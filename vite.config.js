7e716b7ae7487ad0ddc4f46f787555a9d0f056cc
Huynh Le Vu Dat <dat.hlv@suntech.local>
2024-02-22 08:33:42 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: "/render-model-dp",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  // base: "/render-model-dp",
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "./src"),
+  //   },
+  // },
   // server: {
   //   host: "127.0.0.1", // replace with the IP address of the Homestead machine
   //   https: false,
@@ -20,9 +20,21 @@ export default defineConfig({
   //   },
   // },
   // server: {
-  //   origin: "http://127.0.0.1:5173",
+  //   origin: "localhost:5173",
   // },
   // build: {
   //   outDir: "../dist", // Change 'custom-dist' to your desired output directory
+  // },
+  // server: {
+  //   // Listen on all addresses inside the container
+  //   host: true,
+  //   hmr: {
+  //     // Force the Vite client to connect via SSL
+  //     // This will also force a "https://" URL in the hot file
+  //     protocol: "wss",
+  //     // The host where the Vite dev server can be accessed
+  //     // This will also force this host to be written to the hot file
+  //     host: "https://hlvudat1206.github.io/",
+  //   },
   // },
 });
